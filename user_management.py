340d7afc287144edbe67b17e7c079e130acd40aa
Ankush7-cloud <ankushchowdhury01@gmail.com>
2025-06-30 11:19:51 +0530

    Create user_management.py

---
+++ user_management.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from db import init_db
+
+def user_management():
+    st.title("User Management")
+    conn = init_db()
+
+    role = st.session_state.get("role")
+    if role == "admin":
+        users = conn.execute("SELECT username, email, role FROM users").fetchall()
+    else:
+        users = conn.execute("SELECT username, email, role FROM users WHERE role = 'user'").fetchall()
+
+    st.table(users)
