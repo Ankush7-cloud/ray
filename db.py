783d412e292954949340a882bf4f29774bfdf7a7
Ankush7-cloud <ankushchowdhury01@gmail.com>
2025-06-30 11:04:50 +0530

    Create db.py

---
+++ db.py
@@ -0,0 +1,24 @@
+import duckdb
+
+def init_db():
+    conn = duckdb.connect("app_data.db")
+
+    conn.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            username TEXT PRIMARY KEY,
+            email TEXT,
+            password TEXT,
+            role TEXT CHECK(role IN ('admin', 'user'))
+        )
+    """)
+
+    conn.execute("""
+        CREATE TABLE IF NOT EXISTS devices (
+            service_tag TEXT PRIMARY KEY,
+            employee_id TEXT,
+            device_type TEXT,
+            memory TEXT
+        )
+    """)
+
+    return conn
