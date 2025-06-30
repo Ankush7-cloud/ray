ac3ea552f26acd2fb5fb5262a801d32e86c185df
Ankush7-cloud <ankushchowdhury01@gmail.com>
2025-06-30 11:47:08 +0530

    Update resource_management.py

--- resource_management.py
+++ resource_management.py
@@ -5,4 +5,4 @@ def resource_management():
     st.title("Resource Management")
     conn = init_db()
     rows = conn.execute("SELECT * FROM devices").fetchall()
-    st.table(rows)
+    st.table(rows)
