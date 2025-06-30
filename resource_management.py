e15dbea9dd4a01115e8965bf7524da4afa0028ae
Ankush7-cloud <ankushchowdhury01@gmail.com>
2025-06-30 11:20:57 +0530

    Create resource_management.py

---
+++ resource_management.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from db import init_db
+
+def resource_management():
+    st.title("Resource Management")
+    conn = init_db()
+    rows = conn.execute("SELECT * FROM devices").fetchall()
+    st.table(rows)
